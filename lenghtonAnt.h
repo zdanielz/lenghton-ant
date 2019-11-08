196634720a74fc76a8531528e2e5b3f8b22760d4
zdanielz <43722126+zdanielz@users.noreply.github.com>
2019-11-08 17:21:43 +0300

    Update lenghtonAnt.h

--- lenghtonAnt.h
+++ lenghtonAnt.h
@@ -2,6 +2,11 @@
 #include <windows.h>
 using namespace std;
 
+struct vectorAnt {
+	int x;
+	int y;
+};
+
 void CreatRect(HDC dc, int nx, int ny, int SizeOfPart, int width, int height, int padding, HBITMAP hbm) {
 	const int size = 4;
 	POINT p[size];
@@ -14,140 +19,121 @@ void CreatRect(HDC dc, int nx, int ny, int SizeOfPart, int width, int height, in
 	Polygon(dc, p, size);
 }
 
-struct vectorAnt {
-	int x;
-	int y;
-};
-
 class lengtonAnt {
-	public:
-		lengtonAnt(HDC mainDc_, HDC buferDC_, HBITMAP hbm_, int xn, int yn, vectorAnt vect_, int width_, int height_, int padding_, int sizeOfPart) {
-			mainDc = mainDc_;
-			buferDC = buferDC_;
-			hbm = hbm_;
-			x = xn;
-			y = yn;
-			width = width_;
-			height = height_;
-			padding = padding_;
-			SizeOfPart = sizeOfPart;
-			vect = vect_;
+public:
+	lengtonAnt(HDC mainDc_, HDC buferDC_, HBITMAP hbm_, int xn_, int yn_, vectorAnt vect_, int width_, int height_, int padding_, int sizeOfPart, COLORREF MainColor) {
+		mainDc = mainDc_;
+		buferDC = buferDC_;
+		hbm = hbm_;
+		xn = xn_;
+		yn = yn_;
+		width = width_;
+		height = height_;
+		padding = padding_;
+		SizeOfPart = sizeOfPart;
+		vect = vect_;
+		mainColor = MainColor;
+	}
+	void makeMov() {
+		if (WhatColor()) {
+
+			switch (vect.x) {
+			case -1:
+				vect.x = 0;
+				vect.y = -1;
+				break;
+			case 1:
+				vect.x = 0;
+				vect.y = 1;
+				break;
+			case 0:
+				switch (vect.y) {
+				case -1:
+					vect.y = 0;
+					vect.x = 1;
+					break;
+				case 1:
+					vect.y = 0;
+					vect.x = -1;
+					break;
+				}
+			}
+
+			SelectObject(buferDC, hbm);
+
+			color = RGB(226, 124, 62);
+			hBrush = CreateSolidBrush(color);
+			SelectObject(buferDC, hBrush);
+
+			CreatRect(buferDC, xn, yn, SizeOfPart, width, height, padding, hbm);
+			BitBlt(mainDc, 0, 0, width, height, buferDC, 0, 0, SRCCOPY);
 		}
-		void makeMov() {
-			if (WhatColor()) {
-
-				switch (vect.x) {
+		else {
+			switch (vect.x) {
+			case -1:
+				vect.x = 0;
+				vect.y = 1;
+				break;
+			case 1:
+				vect.x = 0;
+				vect.y = -1;
+				break;
+			case 0:
+				switch (vect.y) {
 				case -1:
-					vect.x = 0;
-					vect.y = -1;
+					vect.y = 0;
+					vect.x = -1;
 					break;
 				case 1:
-					vect.x = 0;
-					vect.y = 1;
-				case 0:
-					switch (vect.y) {
-					case -1:
-						vect.y = 0;
-						vect.x = 1;
-						break;
-					case 1:
-						vect.y = 0;
-						vect.x = -1;
-						break;
-					}
-				}
-
-				SelectObject(buferDC, hbm);
-
-				color = RGB(226, 124, 62);
-				hBrush = CreateSolidBrush(color);
-
-				SelectObject(buferDC, hBrush);
-
-				CreatRect(buferDC, x, y, SizeOfPart, width, height, padding, hbm);
-				
-				switch (vect.x) {
-				case -1:
-					x -= 1;
+					vect.y = 0;
+					vect.x = 1;
 					break;
-				case 1:
-					x += 1;
-					break;
-				case 0:
-					switch (vect.y) {
-					case -1:
-						y -= 1;
-						break;
-					case 1:
-						y += 1;
-						break;
-					}
 				}
 			}
-			else {
-				switch (vect.x) {
-				case -1:
-					vect.x = 0;
-					vect.y = 1;
-					break;
-				case 1:
-					vect.x = 0;
-					vect.y = -1;
-					break;
-				case 0:
-					switch (vect.y) {
-					case -1:
-						vect.y = 0;
-						vect.x = -1;
-						break;
-					case 1:
-						vect.y = 0;
-						vect.x = 1;
-						break;
-					}
-				}
 
-				SelectObject(buferDC, hbm);
+			SelectObject(buferDC, hbm);
 
-				color = RGB(50, 50, 50);
-				hBrush = CreateSolidBrush(color);
+			color = mainColor;
+			hBrush = CreateSolidBrush(color);
+			SelectObject(buferDC, hBrush);
 
-				SelectObject(buferDC, hBrush);
-
-				CreatRect(buferDC, x, y, SizeOfPart, width, height, padding, hbm);
-
-				switch (vect.x) {
-				case -1:
-					x -= 1;
-					break;
-				case 1:
-					x += 1;
-					break;
-				case 0:
-					switch (vect.y) {
-					case -1:
-						y -= 1;
-						break;
-					case 1:
-						y += 1;
-						break;
-					}
-				}
-			}
+			CreatRect(buferDC, xn, yn, SizeOfPart, width, height, padding, hbm);
 			BitBlt(mainDc, 0, 0, width, height, buferDC, 0, 0, SRCCOPY);
 		}
 
-	private:
-		HDC mainDc;
-		HDC buferDC;
-		HBITMAP hbm;
-		COLORREF color;
-		HBRUSH hBrush;
-		vectorAnt vect;
-		int x, y, width, height, padding, SizeOfPart;
-		bool WhatColor() {
-			if ((GetPixel(mainDc, x + (SizeOfPart / 2) + (padding * x + x * SizeOfPart), y + (SizeOfPart / 2) + (padding * y + y * SizeOfPart)) == RGB(50, 50, 50)) |
-				(GetPixel(buferDC, x + (SizeOfPart / 2) + (padding * x + x * SizeOfPart), y + (SizeOfPart / 2) + (padding * y + y * SizeOfPart)) == RGB(50, 50, 50))) { return 1; }
-			else { return 0; }
+		switch (vect.x) {
+		case -1:
+			xn -= 1;
+			break;
+		case 1:
+			xn += 1;
+			break;
+		case 0:
+			switch (vect.y) {
+			case -1:
+				yn -= 1;
+				break;
+			case 1:
+				yn += 1;
+				break;
+			}
 		}
+
+		BitBlt(mainDc, 0, 0, width, height, buferDC, 0, 0, SRCCOPY);
+	}
+
+private:
+	HDC mainDc;
+	HDC buferDC;
+	HBITMAP hbm;
+	COLORREF color, mainColor;
+	HBRUSH hBrush;
+	vectorAnt vect;
+	int xn, yn, width, height, padding, SizeOfPart;
+	bool WhatColor() {
+		if (GetPixel(buferDC, xn + (SizeOfPart / 2) + (padding * xn + xn * SizeOfPart), yn + (SizeOfPart / 2) + (padding * yn + yn * SizeOfPart)) == RGB(50, 50, 50)) {
+			return 1;
+		}
+		else { return 0; }
+	}
 };
