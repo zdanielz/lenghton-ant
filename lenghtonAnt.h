b387da1ba24d6fca69ca02edc927b9fc00a85311
zdanielz <43722126+zdanielz@users.noreply.github.com>
2019-11-10 00:22:12 +0300

    Update lenghtonAnt.h

--- lenghtonAnt.h
+++ lenghtonAnt.h
@@ -37,126 +37,93 @@ public:
 		max_y = max_y_;
 	}
 	bool makeMov() {
-			cout << "==============" << endl;
-			if (WhatColor()) {
-				SelectObject(buferDC, hbm);
+		if (WhatColor()) {
 
-				color = RGB(226, 124, 62);
-				hBrush = CreateSolidBrush(color);
-				SelectObject(buferDC, hBrush);
-
-				CreatRect(buferDC, xn, yn, SizeOfPart, width, height, padding, hbm);
-				cout << "right" << endl;
-
-				switch (vect.x) {
+			switch (vect.x) {
+			case -1:
+				vect.x = 0;
+				vect.y = -1;
+				break;
+			case 1:
+				vect.x = 0;
+				vect.y = 1;
+				break;
+			case 0:
+				switch (vect.y) {
 				case -1:
-					vect.x = 0;
-					vect.y = -1;
+					vect.y = 0;
+					vect.x = 1;
 					break;
 				case 1:
-					vect.x = 0;
-					vect.y = 1;
+					vect.y = 0;
+					vect.x = -1;
 					break;
-				case 0:
-					switch (vect.y) {
-					case -1:
-						vect.y = 0;
-						vect.x = 1;
-						break;
-					case 1:
-						vect.y = 0;
-						vect.x = -1;
-						break;
-					}
 				}
-
-				switch (vect.x) {
-				case -1:
-					xn -= 1;
-					break;
-				case 1:
-					xn += 1;
-					break;
-				case 0:
-					switch (vect.y) {
-					case -1:
-						yn -= 1;
-						break;
-					case 1:
-						yn += 1;
-						break;
-					}
-				}
-
-				cout << "vect.x = " << vect.x << endl;
-				cout << "vect.y = " << vect.y << endl;
-				cout << "x = " << xn << " ; y = " << yn << endl;
-				cout << "wh = " << WhatColor() << endl;
-				cout << "==============" << endl;
-
-				BitBlt(mainDc, 0, 0, width, height, buferDC, 0, 0, SRCCOPY);
-				if (((xn == max_x) & (yn == max_y)) | ((xn == 0) & (yn == 0))) { return 0; }
-				else { return 1; }
 			}
-			else {
-				SelectObject(buferDC, hbm);
 
-				color = mainColor;
-				hBrush = CreateSolidBrush(color);
-				SelectObject(buferDC, hBrush);
+			SelectObject(buferDC, hbm);
 
-				CreatRect(buferDC, xn, yn, SizeOfPart, width, height, padding, hbm);
-				cout << "left" << endl;
+			color = RGB(226, 124, 62);
+			hBrush = CreateSolidBrush(color);
+			SelectObject(buferDC, hBrush);
 
-				switch (vect.x) {
+			CreatRect(buferDC, xn, yn, SizeOfPart, width, height, padding, hbm);
+			DeleteObject(hBrush);
+		}
+		else {
+			switch (vect.x) {
+			case -1:
+				vect.x = 0;
+				vect.y = 1;
+				break;
+			case 1:
+				vect.x = 0;
+				vect.y = -1;
+				break;
+			case 0:
+				switch (vect.y) {
 				case -1:
-					vect.x = 0;
-					vect.y = 1;
+					vect.y = 0;
+					vect.x = -1;
 					break;
 				case 1:
-					vect.x = 0;
-					vect.y = -1;
+					vect.y = 0;
+					vect.x = 1;
 					break;
-				case 0:
-					switch (vect.y) {
-					case -1:
-						vect.y = 0;
-						vect.x = -1;
-						break;
-					case 1:
-						vect.y = 0;
-						vect.x = 1;
-						break;
-					}
 				}
-				switch (vect.x) {
-				case -1:
-					xn -= 1;
-					break;
-				case 1:
-					xn += 1;
-					break;
-				case 0:
-					switch (vect.y) {
-					case -1:
-						yn -= 1;
-						break;
-					case 1:
-						yn += 1;
-						break;
-					}
-				}
-
-				cout << "vect.x = " << vect.x << endl;
-				cout << "vect.y = " << vect.y << endl;
-				cout << "x = " << xn << " ; y = " << yn << endl;
-				cout << "wh = " << WhatColor() << endl;
-				cout << "==============" << endl;
-
-				while (BitBlt(mainDc, 0, 0, width, height, buferDC, 0, 0, SRCCOPY) == 0) {}
-				if (((xn == max_x) & (yn == max_y)) | ((xn == 0) & (yn == 0))) { return 0; }
-				else { return 1; }
 			}
+
+			SelectObject(buferDC, hbm);
+
+			color = mainColor;
+			hBrush = CreateSolidBrush(color);
+			SelectObject(buferDC, hBrush);
+
+			CreatRect(buferDC, xn, yn, SizeOfPart, width, height, padding, hbm);
+			DeleteObject(hBrush);
+		}
+
+		switch (vect.x) {
+		case -1:
+			xn -= 1;
+			break;
+		case 1:
+			xn += 1;
+			break;
+		case 0:
+			switch (vect.y) {
+			case -1:
+				yn -= 1;
+				break;
+			case 1:
+				yn += 1;
+				break;
+			}
+		}
+
+		while (BitBlt(mainDc, 0, 0, width, height, buferDC, 0, 0, SRCCOPY) == 0) {}
+		if ((xn + 1 == max_x ) | (yn + 1 == max_y ) | (xn == 0) | (yn == 0)) { return 0; }
+		else { return 1; }
 	}
 
 private:
@@ -168,7 +135,7 @@ private:
 	vectorAnt vect;
 	int xn, yn, width, height, padding, SizeOfPart, max_x, max_y;
 	bool WhatColor() {
-		if (GetPixel(buferDC, xn + (SizeOfPart - SizeOfPart / 2) + (padding * xn + xn * SizeOfPart), yn + (SizeOfPart - SizeOfPart / 2) + (padding * yn + yn * SizeOfPart)) == mainColor) {
+		if (GetPixel(buferDC, xn + (SizeOfPart / 2) + (padding * xn + xn * SizeOfPart), yn + (SizeOfPart / 2) + (padding * yn + yn * SizeOfPart)) == mainColor) {
 			return 1;
 		}
 		else { return 0; }
