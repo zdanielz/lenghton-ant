0e942681bb175a6e65980a44dd0a43eccd3bfcca
zdanielz <43722126+zdanielz@users.noreply.github.com>
2019-11-10 00:21:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+//#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+
 #include <windows.h>
 #include <iostream>
 #include <time.h>
@@ -10,14 +12,14 @@ int main(void) {
 
 	mainColor = RGB(50, 50, 50);
 
-	int width = GetSystemMetrics(SM_CXSCREEN) / 2;
-	int height = GetSystemMetrics(SM_CYSCREEN) / 2;
+	int width = GetSystemMetrics(SM_CXSCREEN);
+	int height = GetSystemMetrics(SM_CYSCREEN);
 
-	int SizeOfPart = 5;
+	int SizeOfPart = 3;
 	int padding = 1;
 
 	int c = 0;
-	int x_ = width / (SizeOfPart + padding), y_ = height / (SizeOfPart + padding);
+	double x_ = width / (SizeOfPart + padding), y_ = height / (SizeOfPart + padding);
 	vectorAnt vect_mov;
 	vect_mov.x = -1;
 	vect_mov.y = 0;
@@ -34,32 +36,42 @@ int main(void) {
 		}
 	}
 
-	HDC mainDc = GetDC(NULL);
+	HDC maindc = GetDC(NULL);
+	HWND hWnd = GetDesktopWindow();
 
-	HDC buferDc = CreateCompatibleDC(mainDc);
-	HBITMAP Bitmap = CreateCompatibleBitmap(mainDc, width, height);
 
-	SelectObject(mainDc, Bitmap);
+	HDC buferDc = CreateCompatibleDC(maindc);
+	HBITMAP Bitmap = CreateCompatibleBitmap(maindc, width, height);
+
 	SelectObject(buferDc, Bitmap);
 
 	HBRUSH hBrush;
 	//color = RGB(226, 124, 62);
 	hBrush = CreateSolidBrush(mainColor);
-
 	SelectObject(buferDc, hBrush);
 
 	for (size_t y = 0; y < y_; y++) {
 		for (size_t x = 0; x < x_; x++) {
-			CreatRect(buferDc, x, y, SizeOfPart, width, height, padding, Bitmap);
+			if((x > x_ / 6) & (x < x_ / 6 * 4) & (y > y_ / 6) & (y < y_ / 6 * 4) & 
+				((x - 2 < x_ / 6) | (x + 2 > x_ / 6 * 4) | (y - 2 < y_ / 6) | (y + 2 > y_ / 6 * 4))){
+				COLORREF color = RGB(226, 124, 62);
+				hBrush = CreateSolidBrush(color);
+				SelectObject(buferDc, hBrush);
+				CreatRect(buferDc, x, y, SizeOfPart, width, height, padding, Bitmap);
+				DeleteObject(hBrush);
+			}
+			else {
+				hBrush = CreateSolidBrush(mainColor);
+				SelectObject(buferDc, hBrush);
+				CreatRect(buferDc, x, y, SizeOfPart, width, height, padding, Bitmap);
+				DeleteObject(hBrush);
+			}
 		}
 	}
 
-	BitBlt(mainDc, 0, 0, width, height, buferDc, 0, 0, SRCCOPY);
+	lengtonAnt Ant(maindc, buferDc, Bitmap, x_ / 2.5, y_ / 2.5, x_, y_, vect_mov, width, height, padding, SizeOfPart, mainColor);
 
-	lengtonAnt Ant(mainDc, buferDc, Bitmap, x_ / 2.5, y_ / 2.5, x_, y_, vect_mov, width, height, padding, SizeOfPart, mainColor);
-
-	int i = 1;
-	while(i) { i = Ant.makeMov(); }
+	while (Ant.makeMov()) {}
 
 	DeleteDC(buferDc);
 	DeleteObject(Bitmap);
